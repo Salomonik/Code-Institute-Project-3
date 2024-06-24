89bf2dab8bd53c0f9da76e06824b6754f89d7bf6
Salomonik <salomonik@gmail.com>
2024-06-24 18:01:29 +0100

    setup env.py for twitch API

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ['TWITCH_CLIENT_ID'] = 'uo0gzrbsiyigb5u0fx6xfrp0t041g1'
+os.environ['TWITCH_CLIENT_SECRET'] = '35qoruf4uhapucyooayfilggb96rex'
+os.environ['IP'] = '0.0.0.0'
+os.environ['PORT'] = '5000'
