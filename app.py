8e441395b00966ff88164701b0c615d97689e543
Salomonik <salomonik@gmail.com>
2024-06-24 18:03:06 +0100

    add app.py token access from API

--- app.py
+++ app.py
@@ -6,3 +6,16 @@ app = Flask(__name__)
 
 CLIENT_ID = os.environ.get('TWITCH_CLIENT_ID')
 CLIENT_SECRET = os.environ.get('TWITCH_CLIENT_SECRET')
+
+def get_igdb_access_token(client_id, client_secret):
+    url = 'https://id.twitch.tv/oauth2/token'
+    payload = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'client_credentials'
+    }
+    response = requests.post(url, data=payload)
+    response.raise_for_status()
+    return response.json()['access_token']
+
+access_token = get_igdb_access_token(CLIENT_ID, CLIENT_SECRET)
