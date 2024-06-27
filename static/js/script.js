5c8ed970eacaec2e482d60adb409b3cdf5cf31c2
Salomonik <salomonik@gmail.com>
2024-06-27 14:36:55 +0100

    add carousel script

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.materialboxed');
     M.Materialbox.init(elems);
+
+    var carouselElems = document.querySelectorAll('.carousel');
+    M.Carousel.init(carouselElems, {
+        fullWidth: true,
+        indicators: true
+    });
 });
