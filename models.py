18cc0f89a8c3c0cf36439037e52122e002f571dd
Salomonik <salomonik@gmail.com>
2024-07-02 16:04:23 +0100

    add GameGenre to models.py

--- models.py
+++ models.py
@@ -47,5 +47,9 @@ class Friend(db.Model):
     friend_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
     created_at = db.Column(db.DateTime, defaulf = datetime.UTC)
     
-
+class GameGenre(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64), unique = True, nullable = False)
+    
+    
     
