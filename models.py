03c52be9e7d8f802241e309c63f8658d9d917539
Salomonik <salomonik@gmail.com>
2024-07-02 15:38:14 +0100

    add User class to models.py

--- models.py
+++ models.py
@@ -1,3 +1,15 @@
 from . import db
 from datetime import datetime
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), unique = True, nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    password_hash = db.Column(db.String(128), nullable = False)
+    created_at = db.Column(db.DateTime, default = datetime.UTC)
+    
+    def __repr__(self):
+        return f'<User {self.username}>'
+    
+
+    
