fe552fcab3ccd861de76291bea7e9f4bc9c6582a
Salomonik <salomonik@gmail.com>
2024-07-03 10:27:27 +0100

    add validation for username in forms.py

--- project/forms.py
+++ project/forms.py
@@ -10,3 +10,8 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
     
+def validate_username(self, email):
+    user = User.query.filter_by(email=email.data).first()
+    if user:
+        raise ValidationError('That username is taken. Please choose different one.')
+
