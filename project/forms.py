c1db167d85a9ee98a5bd69bbaff3ee46a18c6c92
Salomonik <salomonik@gmail.com>
2024-07-03 10:24:26 +0100

    add registration form to forms.py

--- project/forms.py
+++ project/forms.py
@@ -3,3 +3,10 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from project.models import User
 
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+    
