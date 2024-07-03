012dc461cf5a47aaded1f82c1b4bd31e338cab9e
Salomonik <salomonik@gmail.com>
2024-07-03 10:33:52 +0100

    add login form in forms.py

--- project/forms.py
+++ project/forms.py
@@ -5,7 +5,7 @@ from project.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
@@ -19,3 +19,9 @@ def validate_email(self, email):
     user = User.query.filter_by(email = email.data).first()
     if user:
         raise ValidationError('That email is taken. Please choose different one.')
+    
+def LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
