7856d78b212914005d5adec83864df382602529c
Salomonik <salomonik@gmail.com>
2024-07-17 18:28:48 +0100

    modify run.py

--- run.py
+++ run.py
@@ -10,6 +10,6 @@ app = create_app()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=os.environ.get("DEBUG"))
+    app.run(host=os.environ.get("IP", "0.0.0.0"), port=int(os.environ.get("PORT", 5000)), debug=os.environ.get("DEBUG", False))
 
 
